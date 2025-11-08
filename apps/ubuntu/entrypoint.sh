17e30ee12991291a3ddd69452445688d5e5e7cfc
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-08 20:10:45 +0100

    adding initial code for post-run

--- apps/ubuntu/entrypoint.sh
+++ apps/ubuntu/entrypoint.sh
@@ -1,5 +1,85 @@
 #!/usr/bin/env bash
 
+run_main() {
+    local background=${1:-false}
+    shift
+
+    if [ -x /start.sh ]; then
+        echo "[entrypoint] Info: Executing /start.sh"
+        if [ "$background" = true ]; then
+            echo "[entrypoint] Info: Executing /start.sh in background"
+            /start.sh "$@" &
+            MAIN_PID=$!
+        else
+            exec /start.sh "$@"
+        fi
+    elif [ -e /start.sh ]; then
+        echo "[entrypoint] Error: /start.sh exists but is not executable" >&2
+        exit 1
+    elif [ "$#" -gt 0 ]; then
+        echo "[entrypoint] Info: Executing passed command: $*"
+        if [ "$background" = true ]; then
+            echo "[entrypoint] Info: Executing passed command in background"
+            "$@" &
+            MAIN_PID=$!
+        else
+            exec "$@"
+        fi
+    else
+        echo "[entrypoint] Error: No /start.sh and no command provided" >&2
+        exit 1
+    fi
+}
+
+run_scripts() {
+    local runtime=${1:-false}
+    shift
+    [ "$runtime" = true ] && scriptname="runtime" || scriptname="init"
+
+# Process /docker-entrypoint.d/init/ if it exists and is not empty
+if [ -d "/docker-entrypoint.d/${scriptname}" ] && /usr/bin/find "/docker-entrypoint.d/${scriptname}" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read -r v; then
+    if [ "$runtime" = true ]; then
+      run_main true -- "$@"
+    fi
+
+    echo "[entrypoint] Processing ${scriptname} scripts..."
+
+    find "/docker-entrypoint.d/${scriptname}" -follow -type f -print | sort -V | while read -r f; do
+        case "$f" in
+            *.envsh)
+                if [ -x "$f" ]; then
+                    echo "[entrypoint] Sourcing $f"
+                    . "$f"
+                else
+                    echo "[entrypoint] Ignoring $f, not executable"
+                fi
+                ;;
+            *.sh)
+                if [ -x "$f" ]; then
+                    echo "[entrypoint] Running $f"
+                    "$f"
+                else
+                    echo "[entrypoint] Ignoring $f, not executable"
+                fi
+                ;;
+            *) echo "[entrypoint] Ignoring $f";;
+        esac
+    done
+
+    echo "[entrypoint] Execution of ${scriptname} scripts completed..."
+else
+    echo "[entrypoint] No files found in /docker-entrypoint.d/${scriptname}/, skipping"
+
+    if [ "$runtime" = true ]; then
+      run_main false -- "$@"
+    fi
+fi
+
+if [ "$runtime" = true ] && [ -n "$MAIN_PID" ]; then
+    wait "$MAIN_PID"
+fi
+}
+
 echo "
 Welcome to a TrueForge ContainerForge container,
 You are entering the vicinity of an area adjacent to a location.
@@ -11,85 +91,11 @@ These are just examples; it could also be something much better.
 * Questions should be discussed in Discord
 "
 
+
 # Ensure target directory exists
 mkdir -p /docker-entrypoint.d/init /docker-entrypoint.d/runtime
 
 # Copy scripts without overwriting
 cp -rn /customscripts/* /docker-entrypoint.d/
 
-# Process /docker-entrypoint.d/init/ if it exists and is not empty
-if [ -d "/docker-entrypoint.d/init" ] && /usr/bin/find "/docker-entrypoint.d/init" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read -r v; then
-    echo "[entrypoint] Processing init scripts..."
-
-    find "/docker-entrypoint.d/init" -follow -type f -print | sort -V | while read -r f; do
-        case "$f" in
-            *.envsh)
-                if [ -x "$f" ]; then
-                    echo "[entrypoint] Sourcing $f"
-                    . "$f"
-                else
-                    echo "[entrypoint] Ignoring $f, not executable"
-                fi
-                ;;
-            *.sh)
-                if [ -x "$f" ]; then
-                    echo "[entrypoint] Running $f"
-                    "$f"
-                else
-                    echo "[entrypoint] Ignoring $f, not executable"
-                fi
-                ;;
-            *) echo "[entrypoint] Ignoring $f";;
-        esac
-    done
-
-    echo "[entrypoint] Configuration complete"
-else
-    echo "[entrypoint] No files found in /docker-entrypoint.d/init/, skipping"
-fi
-
-# Run main application
-if [ -x /start.sh ]; then
-    echo "[entrypoint] Info: Executing /start.sh"
-    exec /start.sh "$@"
-elif [ -e /start.sh ]; then
-    echo "[entrypoint] Error: /start.sh exists but is not executable" >&2
-    exit 1
-elif [ "$#" -gt 0 ]; then
-    echo "[entrypoint] Info: Executing passed command: $*"
-    exec "$@"
-else
-    echo "[entrypoint] Error: No /start.sh and no command provided" >&2
-    exit 1
-fi
-
-# Process /docker-entrypoint.d/runtime/ if it exists and is not empty
-if [ -d "/docker-entrypoint.d/runtime" ] && /usr/bin/find "/docker-entrypoint.d/runtime" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read -r v; then
-    echo "[entrypoint] Processing runtime scripts..."
-
-    find "/docker-entrypoint.d/runtime" -follow -type f -print | sort -V | while read -r f; do
-        case "$f" in
-            *.envsh)
-                if [ -x "$f" ]; then
-                    echo "[entrypoint] Sourcing $f"
-                    . "$f"
-                else
-                    echo "[entrypoint] Ignoring $f, not executable"
-                fi
-                ;;
-            *.sh)
-                if [ -x "$f" ]; then
-                    echo "[entrypoint] Running $f"
-                    "$f"
-                else
-                    echo "[entrypoint] Ignoring $f, not executable"
-                fi
-                ;;
-            *) echo "[entrypoint] Ignoring $f";;
-        esac
-    done
-
-    echo "[entrypoint] Configuration complete"
-else
-    echo "[entrypoint] No files found in /docker-entrypoint.d/runtime/, skipping"
-fi
+run_scripts false
