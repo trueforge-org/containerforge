99f607b1075c8b56e5622e424f82b71a2980f6e0
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-15 10:44:25 +0100

    some fixes/cleanup

--- apps/autoscan/start.sh
+++ apps/autoscan/start.sh
@@ -1,3 +1,4 @@
+cp -n /app/config.yml /config/config.yml
 AUTOSCAN_CONFIG="/config/config.yml"    && export AUTOSCAN_CONFIG
 AUTOSCAN_DATABASE="/config/autoscan.db" && export AUTOSCAN_DATABASE
 AUTOSCAN_LOG="/config/autoscan.log"     && export AUTOSCAN_LOG
