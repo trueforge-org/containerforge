129238032dde6c20d733c48b6d604f7cf502d6ad
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-15 19:26:00 +0100

    more

--- apps/duplicacy/start.sh
+++ apps/duplicacy/start.sh
@@ -1,14 +1,5 @@
-echo "Applying permissions to /cache"
-chmod "=rwx" "/cache"
-find "/cache" -maxdepth 0 \( ! -user hotio -or ! -group hotio \) -exec chown hotio:hotio {} +
-
-echo "Applying permissions to /logs"
-chmod "=rwx" "/logs"
-find "/logs" -maxdepth 0 \( ! -user hotio -or ! -group hotio \) -exec chown hotio:hotio {} +
-
 if [[ ! -f "/config/machine-id" ]]; then
     tr -dc 'a-f0-9' < /dev/urandom | fold -w 32 | head -n 1 > "/config/machine-id"
-    find "/config/machine-id" -maxdepth 0 \( ! -user hotio -or ! -group hotio \) -exec chown hotio:hotio {} +
 fi
 
 if [[ ! -f "/config/settings.json" ]]; then
@@ -17,13 +8,12 @@ if [[ ! -f "/config/settings.json" ]]; then
         "log_directory"         : "/logs",
         "temporary_directory"   : "/cache"
     }' > "/config/settings.json"
-    find "/config/settings.json" -maxdepth 0 \( ! -user hotio -or ! -group hotio \) -exec chown hotio:hotio {} +
 fi
 
 if [[ ! -f "/config/duplicacy.json" ]]; then
     echo '{}' > "/config/duplicacy.json"
-    find "/config/duplicacy.json" -maxdepth 0 \( ! -user hotio -or ! -group hotio \) -exec chown hotio:hotio {} +
 fi
 
 HOME="/app"
+export DUPLICACY_HOME="/config"
 exec "/app/duplicacy_web"
