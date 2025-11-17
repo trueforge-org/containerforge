27037fcb357f80efeacc9a86ac7fbd016188c8b6
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-17 16:04:57 +0100

    hmm check the copy and such

--- apps/kavita/start.sh
+++ apps/kavita/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cp -rn /defaults /config
+if [[ ! -f "/config/appsettings.json" ]]; then
+    cp /defaults/appsettings-init.json /config/
+fi
+
+echo "config content:"
+ls -l config
 
 cd /app
 exec /app/Kavita $@
