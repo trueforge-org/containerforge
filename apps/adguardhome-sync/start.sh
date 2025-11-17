26d4336357f62e7035f81135b13544c019ca8e9b
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-17 23:01:22 +0100

    hmm

--- apps/adguardhome-sync/start.sh
+++ apps/adguardhome-sync/start.sh
@@ -3,5 +3,5 @@
 cp -n /defaults/adguardhome-sync.yaml /config/adguardhome-sync.yaml
 
 cd /app
-exec  /app/adguardhome-sync/adguardhome-sync run --config "${CONFIGFILE:-/config/adguardhome-sync.yaml}"
+exec  /app/adguardhome-sync run --config "${CONFIGFILE:-/config/adguardhome-sync.yaml}"
 
