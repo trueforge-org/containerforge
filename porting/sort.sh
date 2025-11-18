54b15d8726a92c5e9a8e2cf3b8441fe704c1cc49
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-18 17:27:09 +0100

    sorting fixes

--- porting/sort.sh
+++ porting/sort.sh
@@ -1,11 +1,20 @@
-mkdir -p ./denied-apk ./denied-from
+mkdir -p ./denied-apk ./denied-from ./denied-root ./good ./denied-dockerlines/  ./denied-startlines/
 
-for dir in ./processed/*/; do
+for dir in ./post-processed/*/; do
+startlines=$(wc -l < "$dir/start.sh")
+dockerlines=$(wc -l < "$dir/Dockerfile")
+# Check if more than 200
     if [ -f "$dir/Dockerfile" ] && grep -q "apk " "$dir/Dockerfile"; then
         mv "$dir" ./denied-apk/
-    fi
-    if [ -f "$dir/Dockerfile" ] && grep -q "FROM ghcr.io/linuxserver/" "$dir/Dockerfile"; then
+    elif [ -f "$dir/Dockerfile" ] && grep -q "FROM ghcr.io/linuxserver/" "$dir/Dockerfile"; then
         mv "$dir" ./denied-from/
-    fi
-
+    elif [ -d "$dir/root" ]; then
+        mv "$dir" ./denied-root/
+    elif [ "$startlines" -gt 100 ]; then
+        mv "$dir" ./denied-dockerlines/
+    elif [ "$dockerlines" -gt 150 ]; then
+        mv "$dir" ./denied-startlines/
+    else
+        mv "$dir" ./good/
+fi
 done
