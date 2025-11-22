d235fbd002f4c1cfc8f51f10b015efc577f6b004
Kjeld Schouten <info@kjeldschouten.nl>
2025-11-22 13:00:36 +0100

    fix watchtower healthcheck

--- apps/watchtower/container_test.go
+++ apps/watchtower/container_test.go
@@ -22,7 +22,7 @@ func Test(t *testing.T) {
 
 	app, err := testcontainers.Run(
 		ctx, image,
-		testcontainers.WithCmdArgs("/watchtower", "--health-check"),
+		testcontainers.WithCmdArgs("/app/watchtower", "--health-check"),
 	)
 	testcontainers.CleanupContainer(t, app)
 	require.NoError(t, err)
