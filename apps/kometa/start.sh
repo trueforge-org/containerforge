5a684f7dfc83af93bd86f194cd401888d95c4b84
Kjeld Schouten <info@kjeldschouten.nl>
2025-12-13 16:58:35 +0100

    fix: ensure correct venv location is used

--- apps/kometa/start.sh
+++ apps/kometa/start.sh
@@ -18,13 +18,13 @@ if [[ -n "${CONFIG_FILE}" ]] && [[ ! -e "${CONFIG_FILE}" ]]; then
 fi
 
 if { echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--run|-r)([\s]|$)'; } && { echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--config|-c)([\s])(.+\/[^\/]+)\.(yml|yaml)'; }; then
-     /app/venv/bin/python3 /app/kometa/kometa.py "${CLI_OPTIONS[@]}"
+     /config/venv/bin/python3 /app/kometa/kometa.py "${CLI_OPTIONS[@]}"
 elif echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--run|-r)([\s]|$)'; then
-     /app/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
+     /config/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
 elif echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--config|-c)([\s])(.+\/[^\/]+)\.(yml|yaml)'; then
-     /app/venv/bin/python3 /app/kometa/kometa.py --run "${CLI_OPTIONS[@]}"
+     /config/venv/bin/python3 /app/kometa/kometa.py --run "${CLI_OPTIONS[@]}"
 else
-     /app/venv/bin/python3 /app/kometa/kometa.py --run --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
+     /config/venv/bin/python3 /app/kometa/kometa.py --run --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
 fi
 
 IFS="|" read -r -a CLI_OPTIONS <<< "$CLI_OPTIONS_STRING"
@@ -46,15 +46,15 @@ fi
 
 if { echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--time|-t)([\s])'; } && { echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--config|-c)([\s])(.+\/[^\/]+)\.(yml|yaml)'; }; then
     exec \
-         /app/venv/bin/python3 /app/kometa/kometa.py "${CLI_OPTIONS[@]}"
+         /config/venv/bin/python3 /app/kometa/kometa.py "${CLI_OPTIONS[@]}"
 elif echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--time|-t)([\s])'; then
     exec \
-         /app/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
+         /config/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" "${CLI_OPTIONS[@]}"
 elif echo "${CLI_OPTIONS[@]}" | grep -qPo '([\s]|^)(--config|-c)([\s])(.+\/[^\/]+)\.(yml|yaml)'; then
     exec \
-         /app/venv/bin/python3 /app/kometa/kometa.py --time "${KOMETA_TIME:-03:00}" "${CLI_OPTIONS[@]}"
+         /config/venv/bin/python3 /app/kometa/kometa.py --time "${KOMETA_TIME:-03:00}" "${CLI_OPTIONS[@]}"
 else
     exec \
-         /app/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" --time "${KOMETA_TIME:-03:00}" "${CLI_OPTIONS[@]}"
+         /config/venv/bin/python3 /app/kometa/kometa.py --config "${CONFIG_FILE}" --time "${KOMETA_TIME:-03:00}" "${CLI_OPTIONS[@]}"
 fi
 
