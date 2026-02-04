473ee45525dd1f2895908d3710fdfd555b1e0d61
Alfi0812 <admin@alfi0812.de>
2026-02-04 16:14:05 +0000

    fix renovate

--- apps/devcontainer/container_test.go
+++ apps/devcontainer/container_test.go
@@ -22,7 +22,7 @@ func Test(t *testing.T) {
 
 	app, err := testcontainers.Run(
 		ctx, image,
-		testcontainers.WithCmdArgs("clustertool", "info"),
+		testcontainers.WithCmdArgs("forgetool", "info"),
 	)
 	testcontainers.CleanupContainer(t, app)
 	require.NoError(t, err)
