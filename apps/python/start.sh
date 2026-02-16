78843e7a1d18461b6543445526d64fee21fd3288
Kjeld Schouten <info@kjeldschouten.nl>
2026-02-16 15:06:46 +0100

    fix mistake

--- apps/python/start.sh
+++ apps/python/start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -d /apps/venv ] && { [ ! -d /config/venv ] || [ -z "$(ls -A /config/venv 2>/dev/null)" ]; }; then
+if [ -d /app/venv ] && { [ ! -d /config/venv ] || [ -z "$(ls -A /config/venv 2>/dev/null)" ]; }; then
     echo "[start.sh] Restoring prebuilt venv to /config/venv"
     rm -rf /config/venv
     mkdir -p /config
-    cp -R /apps/venv /config/venv
+    cp -R /app/venv /config/venv
 fi
 
 exec "$@"
