d27abe3b04a0acedf469954484fabd71f01f65c9
Brocklobsta <brock.d.allen@gmail.com>
2026-07-16 15:44:19 -0700

    fix(emby): Update app paths

--- apps/emby/start.sh
+++ apps/emby/start.sh
@@ -2,14 +2,14 @@
 
 APP_DIR="/app/bin"
 
-export AMDGPU_IDS="${APP_DIR}/extra/share/libdrm/amdgpu.ids"
+export AMDGPU_IDS="${APP_DIR}/share/libdrm/amdgpu.ids"
 export FONTCONFIG_PATH="${APP_DIR}/etc/fonts"
-export LD_LIBRARY_PATH="${APP_DIR}/lib:${APP_DIR}/extra/lib"
-export OCL_ICD_VENDORS="${APP_DIR}/extra/etc/OpenCL/vendors"
+export LD_LIBRARY_PATH="${APP_DIR}/lib:${APP_DIR}/lib"
+export OCL_ICD_VENDORS="${APP_DIR}/etc/OpenCL/vendors"
 export PCI_IDS_PATH="${APP_DIR}/share/hwdata/pci.ids"
 export SSL_CERT_FILE="${APP_DIR}/etc/ssl/certs/ca-certificates.crt"
 if [ -d "/lib/x86_64-linux-gnu" ]; then
-    export LIBVA_DRIVERS_PATH="/usr/lib/x86_64-linux-gnu/dri:${APP_DIR}/extra/lib/dri"
+    export LIBVA_DRIVERS_PATH="/usr/lib/x86_64-linux-gnu/dri:${APP_DIR}/lib/dri"
 fi
 
 exec \
